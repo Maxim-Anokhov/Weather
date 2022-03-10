f77419580c49c2118fcfa38d64a55fef68f718c1
Maxim <maxim.anokhov@gmail.com>
2022-03-10 21:55:34 +0700

    finished second tab

--- main.js
+++ main.js
@@ -1,52 +1,53 @@
-import { submitButton, serverUrl, apiKey, temperatura, city_name, atmosfereNow, favoriteButton, feels_like, clouds, sunrise, sunste, } from "./view.js"
+import { favoritList, deleteButton, submitButton, serverUrl, apiKey, temperatura, city_name, atmosfereNow, favoriteButton, feels_like, clouds, sunrise, sunset, } from "./view.js"
 favoriteButton.addEventListener("click", addCity)
 submitButton.addEventListener("click", changeWeater);
-let deleteButton = document.querySelectorAll(".deleteButton")
+deleteButton.forEach(button => button.addEventListener("click", deleteCity))
+favoritList.forEach(city => city.addEventListener("click", enterCityName))
+
 
 function enterCityName(event) {
-    const city = event.currentTarget.innerHTML
-    city = getName()
+    const city = event.currentTarget.textContent;
+    document.querySelector(".inputWindow").value = city;
 }
 
 function getName() {
     const cityName = document.querySelector(".inputWindow").value;
-    console.log(cityName)
     return cityName
-
 }
 
 function addCity() {
     const cityName = getName()
+    const element = `<li class="location"> <button class="enterCity">${cityName}</button><button class="deleteButton"></button></li>`
     const listLocation = document.querySelector(".listLocation");
-    listLocation.insertAdjacentHTML("afterbegin", `<li class="location"> <button class="enterCity">${cityName}</button><button class="deleteButton"></button></li>`)
+    listLocation.insertAdjacentHTML("afterbegin", element)
     listLocation.firstElementChild.lastElementChild.addEventListener("click", deleteCity);
+    listLocation.firstElementChild.firstElementChild.addEventListener("click", enterCityName)
 }
-deleteButton.forEach(button => button.addEventListener("click", deleteCity))
-
-
 
 function deleteCity(event) {
     const location = event.currentTarget.parentElement;
     location.remove()
-
 }
-
 
 function creatUrl() {
     const cityName = getName()
-    console.log(cityName)
     return `${serverUrl}?q=${cityName}&appid=${apiKey}&units=metric`;
 }
 
 function changeWeater() {
     const url = creatUrl()
-    const responseServ = fetch(url).then(response => response.json())
-    responseServ.then(data => console.log(data))
-    responseServ.then(data => city_name.forEach(item => item.innerHTML = data.name));
-    responseServ.then(data => temperatura.forEach(item => item.innerHTML = Math.round(data.main.temp) + "&#176;"));
-    responseServ.then(data => atmosfereNow.src = `http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`);
-    responseServ.then(data => feels_like.innerHTML = `Feelse like: ${data.main.feels_like }&#176;`);
-    responseServ.then(data => clouds.innerHTML = `Weather: ${data.weather[0].main}`);
-    responseServ.then(data => sunrise.innerHTML = `Sunrise: ${data.sys.sunrise}`)
-    responseServ.then(data => sunste.innerHTML = `Sunset: ${data.sys.sunset}`)
+    const respServ = fetch(url).then(response => response.json());
+    let options = { hour: "numeric", minute: "numeric" }
+    respServ.then(data => city_name.forEach(item => item.innerHTML = data.name));
+    respServ.then(data => temperatura.forEach(item => item.innerHTML = Math.round(data.main.temp) + "&#176;"));
+    respServ.then(data => atmosfereNow.src = `http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`);
+    respServ.then(data => feels_like.innerHTML = `Feelse like: ${data.main.feels_like }&#176;`);
+    respServ.then(data => clouds.innerHTML = `Weather: ${data.weather[0].main}`);
+    respServ.then(data => new Date(data.sys.sunrise))
+        .then(data => data.toLocaleTimeString('ru-RU', options))
+        .then(data => sunrise.innerHTML = `Sunrise: ${data}`)
+    respServ.then(data => new Date(data.sys.sunset))
+        .then(data => data.toLocaleTimeString('UTC', options))
+        .then(data => sunset.innerHTML = `Sunset: ${data}`)
+
 }
--- view.js
+++ view.js
@@ -1,13 +1,15 @@
 const submitButton = document.querySelector(".header");
+const favoriteButton = document.querySelector(".heart");
+const deleteButton = document.querySelectorAll(".deleteButton")
 const serverUrl = 'http://api.openweathermap.org/data/2.5/weather';
 const apiKey = "f660a2fb1e4bad108d6160b7f58c555f";
 const temperatura = document.querySelectorAll(".temp");
 const city_name = document.querySelectorAll(".city_name");
 const atmosfereNow = document.querySelector(".atmosfere");
-const favoriteButton = document.querySelector(".heart");
 const feels_like = document.querySelector(".feels");
 const clouds = document.querySelector(".clouds");
 const sunrise = document.querySelector('.sunrise');
-const sunste = document.querySelector('.sunset');
-
-export { submitButton, serverUrl, apiKey, temperatura, city_name, atmosfereNow, favoriteButton, feels_like, clouds, sunrise, sunste, };
+const sunset = document.querySelector('.sunset');
+const favoritList = document.querySelectorAll(".enterCity");
+const BUTTONS = [submitButton, favoriteButton, deleteButton]
+export { favoritList, deleteButton, submitButton, serverUrl, apiKey, temperatura, city_name, atmosfereNow, favoriteButton, feels_like, clouds, sunrise, sunset, };
