dea90407948f5d4a1c323cfc943b90fab5f1361a
Maxim <maxim.anokhov@gmail.com>
2022-03-15 15:35:34 +0700

    chenge tab Forecast

--- main.js
+++ main.js
@@ -1,33 +1,20 @@
-import {
-    favoritList,
-    deleteButton,
-    submitButton,
-    serverUrl,
-    apiKey,
-    temperatura,
-    city_name,
-    atmosfereNow,
-    favoriteButton,
-    feels_like,
-    clouds,
-    sunrise,
-    sunset,
-    dates
-} from "./view.js"
+import { BUTTONS, UI_ELEMENTS, serverUrl, apiKey, } from "./view.js"
 
-favoriteButton.addEventListener("click", addCity);
+BUTTONS.FAVORIT_BUTTON.addEventListener("click", checkCity);
+BUTTONS.SUBMIT_BUTTON.addEventListener("click", changeWeater);
+BUTTONS.DELETE_BUTTON.forEach(button => button.addEventListener("click", deleteCity));
+UI_ELEMENTS.FAVORIT_LIST.forEach(city => city.addEventListener("click", getFavoritCity));
 
-submitButton.addEventListener("click", changeWeater);
+function checkCity() {
+    let cityName = getName()
+    let newArray = []
+    const favoritList = document.querySelectorAll(".enterCity")
+    favoritList.forEach(city => newArray.push(city.textContent))
+    const favoritCity = newArray.find(city => city === cityName);
+    console.log(favoritCity);
+    !favoritCity ? addCity() : "";
+}
 
-deleteButton.forEach(button => button.addEventListener("click", deleteCity));
-
-favoritList.forEach(city => city.addEventListener("click", getFavoritCity));
-
-// function checkCity() {
-//     const cityName = getName()
-//     favoritList.forEach(city => { if (city.textContent !== cityName) { console.log(favoriteButton) } })
-// }
-// checkCity()
 
 function getFavoritCity(event) {
     const city = event.currentTarget.textContent;
@@ -37,18 +24,21 @@ function getFavoritCity(event) {
 
 function getName() {
     const cityName = document.querySelector(".inputWindow").value;
+
     return cityName;
 }
 
 function addCity() {
-    const cityName = getName();
+
+    const cityName = getName()
     const element = `<li class="location"> 
     <button class="enterCity">${cityName}</button>
     <button class="deleteButton"></button></li>`
     const listLocation = document.querySelector(".listLocation");
     listLocation.insertAdjacentHTML("afterbegin", element);
     listLocation.firstElementChild.lastElementChild.addEventListener("click", deleteCity);
-    listLocation.firstElementChild.firstElementChild.addEventListener("click", enterCityName);
+    listLocation.firstElementChild.firstElementChild.addEventListener("click", getFavoritCity);
+
 }
 
 function deleteCity(event) {
@@ -88,9 +78,9 @@ function weaterNow() {
         const temp = Math.round(data.main.temp) + "&#176;";
         const clouds = `${url}${data.weather[0].icon}@2x.png`;
         const cityName = data.name;
-        temperatura.forEach(item => item.innerHTML = temp);
-        atmosfereNow.src = clouds;
-        city_name.forEach(item => item.innerHTML = cityName);
+        UI_ELEMENTS.TEMPERATURA.forEach(item => item.innerHTML = temp);
+        UI_ELEMENTS.ATMOSFERE_NOW.src = clouds;
+        UI_ELEMENTS.CITY_NAME.forEach(item => item.innerHTML = cityName);
     })
 }
 
@@ -103,12 +93,11 @@ function weaterDetails() {
         const weather = `Weather: ${data.weather[0].main}`;
         const sunriseData = new Date(data.sys.sunrise).toLocaleTimeString('ru-RU', options);
         const sunsetData = new Date(data.sys.sunset).toLocaleTimeString('UTC', options);
-        feels_like.innerHTML = feelsLike;
-        clouds.innerHTML = weather;
-        sunrise.innerHTML = `Sunrise: ${ sunriseData}`;
-        sunset.innerHTML = `Sunset: ${ sunsetData}`;
+        UI_ELEMENTS.FEELS_LIKE.innerHTML = feelsLike;
+        UI_ELEMENTS.CLOUDS.innerHTML = weather;
+        UI_ELEMENTS.SUNRISE.innerHTML = `Sunrise: ${ sunriseData}`;
+        UI_ELEMENTS.SUNSET.innerHTML = `Sunset: ${ sunsetData}`;
     })
-
 }
 
 function weaterForecast() {
@@ -116,55 +105,34 @@ function weaterForecast() {
     const response = responseForecast();
     const optionsDate = { day: "numeric", month: "long" }
     const optionsTime = { hour: "numeric", minute: "numeric" }
-
+    const url = "https://openweathermap.org/img/wn/";
+    const forecast = document.querySelector(".forecast");
+    const deleteForecast = document.querySelectorAll(".forecastWindow");
+    deleteForecast.forEach(forecast => forecast.remove());
+    let parametrsArray = []
     response.then(data => data.list).then(data => {
-        const url = "https://openweathermap.org/img/wn/"
-        const forecast = document.querySelector(".infoWeatherForecast")
-        document.querySelector(".forecast").remove()
-        const div = ` <div class="forecast">
-        <div class="forecastWindow">
-           <li class="date">${(new Date(data[0].dt_txt))
-               .toLocaleString('EN-en', optionsDate)}</li>   
-           <li class="time">${(new Date(data[0].dt_txt))
-               .toLocaleString('ru', optionsTime)}</li>
-           <li class="temper">Temperature:${Math.round(data[0].main.temp)}&#176;</li>
-           <li class="feelsLike">Feels Like: ${Math.round(data[0].main.feels_like)}&#176;</li>
-           <li class="weatherconditions ">${data[0].weather[0].main}
-           <img class="img"src="${url}${data[0].weather[0].icon}@2x.png"></li>
-        </div>
-        <div class="forecastWindow">
-           <li class="date">${(new Date(data[1].dt_txt))
-               .toLocaleString('EN-en', optionsDate)}</li>   
-           <li class="time">${(new Date(data[1].dt_txt))
-               .toLocaleString('ru', optionsTime)}</li>
-           <li class="temper">Temperature:${Math.round(data[1].main.temp)}&#176;</li>
-           <li class="feelsLike">Feels Like: ${Math.round(data[1].main.feels_like)}&#176;</li>
-           <li class="weatherconditions">${data[1].weather[0].main}
-           <img class="img"src="${url}${data[1].weather[0].icon}@2x.png"></li>
-        </div>
-        <div class="forecastWindow">
-           <li class="date">${(new Date(data[2].dt_txt))
-               .toLocaleString('EN-en', optionsDate)}</li>   
-           <li class="time">${(new Date(data[2].dt_txt))
-               .toLocaleString('ru', optionsTime)}</li>
-           <li class="temper">Temperature:${Math.round(data[2].main.temp)}&#176;</li>
-           <li class="feelsLike">Feels Like: ${Math.round(data[2].main.feels_like)}&#176;</li>
-           <li class="weatherconditions">${data[2].weather[0].main}
-           <img class="img"src="${url}${data[2].weather[0].icon}@2x.png" alt=""></li>
-        </div>
-        <div class="forecastWindow">
-           <li class="date">${(new Date(data[3].dt_txt))
-               .toLocaleString('EN-en', optionsDate)}</li>   
-           <li class="time">${(new Date(data[3].dt_txt))
-               .toLocaleString('ru', optionsTime)}</li>
-           <li class="temper">Temperature:${Math.round(data[3].main.temp)}&#176;</li>
-           <li class="feelsLike">Feels Like: ${Math.round(data[3].main.feels_like)}&#176;</li>
-           <li class="weatherconditions">${data[3].weather[0].main}
-           <img class="img"src="${url}${data[3].weather[0].icon}@2x.png" alt=""></li>
-                   </div>
-   </div>`;
-        forecast.insertAdjacentHTML("beforeend", div);
+        for (let order = 0; order < 10; order++) {
+
+            let parametrs
+            parametrsArray.push(parametrs = {
+                date: (new Date(data[order].dt_txt)).toLocaleString('EN-en', optionsDate),
+                time: (new Date(data[order].dt_txt)).toLocaleString('ru', optionsTime),
+                temperature: Math.round(data[order].main.temp),
+                filsLike: Math.round(data[order].main.feels_like),
+                condition: data[order].weather[0].main,
+                img: data[order].weather[0].icon,
+            })
+        }
+        parametrsArray.forEach(parametrs => {
+            const div = `<div class="forecastWindow">
+                           <li class="date">${parametrs.date}</li>   
+                           <li class="time">${parametrs.time}</li>
+                           <li class="temper">Temperature:${parametrs.temperature}&#176;</li>
+                           <li class="feelsLike">Feels Like: ${parametrs.feelsLike}&#176;</li>
+                           <li class="weatherconditions ">${parametrs.condition}
+                           <img class="img"src="${url}${parametrs.img}@2x.png"></li>
+                        </div>`;
+            forecast.insertAdjacentHTML("beforeend", div);
+        })
     });
-
-
 }
--- view.js
+++ view.js
@@ -1,30 +1,19 @@
-const submitButton = document.querySelector(".header");
-const favoriteButton = document.querySelector(".heart");
-const deleteButton = document.querySelectorAll(".deleteButton")
+const BUTTONS = {
+    SUBMIT_BUTTON: document.querySelector(".header"),
+    FAVORIT_BUTTON: document.querySelector(".heart"),
+    DELETE_BUTTON: document.querySelectorAll(".deleteButton"),
+}
+const UI_ELEMENTS = {
+    TEMPERATURA: document.querySelectorAll(".temp"),
+    CITY_NAME: document.querySelectorAll(".city_name"),
+    ATMOSFERE_NOW: document.querySelector(".atmosfere"),
+    FEELS_LIKE: document.querySelector(".feels"),
+    CLOUDS: document.querySelector(".clouds"),
+    SUNRISE: document.querySelector('.sunrise'),
+    SUNSET: document.querySelector('.sunset'),
+    FAVORIT_LIST: document.querySelectorAll(".enterCity"),
+    DATES: document.querySelectorAll(".date")
+}
 const serverUrl = 'https://api.openweathermap.org/data/2.5/weather';
 const apiKey = "97f36208f41daeec8c857deb48d7e06c"
-const temperatura = document.querySelectorAll(".temp");
-const city_name = document.querySelectorAll(".city_name");
-const atmosfereNow = document.querySelector(".atmosfere");
-const feels_like = document.querySelector(".feels");
-const clouds = document.querySelector(".clouds");
-const sunrise = document.querySelector('.sunrise');
-const sunset = document.querySelector('.sunset');
-const favoritList = document.querySelectorAll(".enterCity");
-const dates = document.querySelectorAll(".date")
-export {
-    favoritList,
-    deleteButton,
-    submitButton,
-    serverUrl,
-    apiKey,
-    temperatura,
-    city_name,
-    atmosfereNow,
-    favoriteButton,
-    feels_like,
-    clouds,
-    sunrise,
-    sunset,
-    dates
-};
+export { BUTTONS, UI_ELEMENTS, serverUrl, apiKey, };
